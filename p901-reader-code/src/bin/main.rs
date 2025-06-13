d003a62af43631bfc472e606b368156e4378d6db
jgcodes2020 <j349guo@uwaterloo.ca>
2025-06-13 11:43:26 -0400

    Crude Rust example that I got working on the ESP.

--- p901-reader-code/src/bin/main.rs
+++ p901-reader-code/src/bin/main.rs
@@ -1,30 +1,64 @@
 #![no_std]
 #![no_main]
 
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
-use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
-use log::info;
+use esp_hal::timer::timg::TimerGroup;
+use esp_hal::uart::{AtCmdConfig, Config, RxConfig, Uart};
+use p901_reader_code::read_line_dbg;
+use core::fmt::Write;
+use core::slice;
 
 extern crate alloc;
 
-#[main]
-fn main() -> ! {
+const READ_BUF_SIZE: usize = 64;
+
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
     // generator version: 0.3.1
 
-    esp_println::logger::init_logger_from_env();
-
+    // Initialize the ESP stuff.
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
 
+    // Setup a 72kB heap. This should be big enough for
+    // most dynamic things we need to worry about.
     esp_alloc::heap_allocator!(size: 72 * 1024);
+    
+    // Initialize the async executor.
+    let timer0 = TimerGroup::new(peripherals.TIMG1);
+    esp_hal_embassy::init(timer0.timer0);
+
+    // Radio init code. Won't need this until later.
+    // let timer1 = TimerGroup::new(peripherals.TIMG0);
+    // let _init = esp_wifi::init(
+    //     timer1.timer0,
+    //     esp_hal::rng::Rng::new(peripherals.RNG),
+    //     peripherals.RADIO_CLK,
+    // )
+    // .unwrap();
+
+    // TODO: Embassy lets us execute async tasks in parallel via the spawner.
+    let _ = spawner;
+
+    let uart_cfg = Config::default()
+        .with_rx(RxConfig::default().with_fifo_full_threshold(READ_BUF_SIZE as u16))
+        .with_baudrate(9600);
+
+    // UART0 is the UART connection to the computer
+    let mut uart = Uart::new(peripherals.UART0, uart_cfg)
+        .unwrap()
+        .with_tx(peripherals.GPIO1)
+        .with_rx(peripherals.GPIO3)
+        .into_async();
+    uart.set_at_cmd(AtCmdConfig::default().with_cmd_char(0x04u8));
 
     loop {
-        info!("Hello world!");
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        // This is equivalent to reading/writing from stdin/stdout.
+        write!(&mut uart, "input: ").unwrap();
+        let line = read_line_dbg(&mut uart).await.unwrap();
+        write!(&mut uart, "received: {}\r\n", line).unwrap();
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
 }
--- p901-reader-code/src/lib.rs
+++ p901-reader-code/src/lib.rs
@@ -1,1 +1,65 @@
 #![no_std]
+
+use core::{
+    fmt::{Arguments, Debug, Write},
+    slice,
+};
+
+use alloc::{
+    string::{FromUtf8Error, String},
+    vec::Vec,
+};
+use embedded_io::Error;
+use embedded_io_async::Read;
+
+extern crate alloc;
+
+
+/// Error type returned by [`read_line_dbg`].
+pub enum ReadlineError<T: Read> {
+    /// An I/O error occured on the underlying stream.
+    IO(T::Error),
+    /// The received line isn't valid UTF-8.
+    Utf8(FromUtf8Error),
+}
+
+impl<T: Read> Error for ReadlineError<T> {
+    fn kind(&self) -> embedded_io::ErrorKind {
+        match self {
+            ReadlineError::IO(io) => io.kind(),
+            ReadlineError::Utf8(_) => embedded_io::ErrorKind::InvalidData,
+        }
+    }
+}
+
+impl<T: Read> Debug for ReadlineError<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::IO(arg0) => f.debug_tuple("IO").field(arg0).finish(),
+            Self::Utf8(arg0) => f.debug_tuple("Utf8").field(arg0).finish(),
+        }
+    }
+}
+
+/// Crude function for reading lines from any input stream.
+/// This is mainly for reading/writing to the computer serial over UART0.
+pub async fn read_line_dbg<T: Read>(src: &mut T) -> Result<String, ReadlineError<T>> {
+    let mut next = 0u8;
+    let mut buffer = Vec::<u8>::new();
+    // Read bits one at a time until either an I/O error happens or we hit a newline.
+    while {
+        src.read(slice::from_mut(&mut next))
+            .await
+            .map_err(ReadlineError::IO)?;
+        next != b'\r'
+    } {
+        match next {
+            // ignore non-printable characters for now.
+            0..31 | 127 => (),
+            _ => buffer.push(next)
+        }
+    }
+    // Convert the byte buffer to a string buffer.
+    let sbuffer = String::from_utf8(buffer).map_err(ReadlineError::Utf8)?;
+    Ok(sbuffer)
+}
